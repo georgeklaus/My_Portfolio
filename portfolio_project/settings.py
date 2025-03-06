9e8d87ea09ed8a8c7de333f171a8b21530dfdf7c
george <georgerubinga@gmail.com>
2025-03-06 18:08:28 +0300

    updated the file directory

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / '/staticfiles']  # Add static files directory
+STATICFILES_DIRS = [BASE_DIR / '/static']  # Add static files directory
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
 
 # Default primary key field type
