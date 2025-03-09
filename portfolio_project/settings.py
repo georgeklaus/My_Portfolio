e0e90b91f67d0d8a483467d6893d3c733b1eda53
george <georgerubinga@gmail.com>
2025-03-09 19:48:20 +0300

    updated again my settingsi

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -70,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'portfolio_project.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
--- portfolio_project/wsgi.py
+++ portfolio_project/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Portfolio_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio_project.settings')
 
 application = get_wsgi_application()
 app = application
