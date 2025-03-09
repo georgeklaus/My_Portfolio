d849cc45d5169f36b5342c6f5c9c45746f1b2482
george <georgerubinga@gmail.com>
2025-03-09 20:27:24 +0300

    Fix static files configuration

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -42,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,8 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']  # Add static files directory
-STATIC_ROOT = BASE_DIR / 'staticfiles/admin'  # Add this line
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Ensure this exists
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Set correct static root
+
+# WhiteNoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
