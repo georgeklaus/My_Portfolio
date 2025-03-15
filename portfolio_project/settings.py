69e669b0cd9157b2aac20f0716ff31816c590aca
george <georgerubinga@gmail.com>
2025-03-15 15:02:13 +0300

    changed my project to my project

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio',  # Add your portfolio app here
+    'my_portfolio',  # Add your portfolio app here
 ]
 
 MIDDLEWARE = [
